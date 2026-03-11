07ffb5813fbc388090ef485fd0575174777a0166
Brawnyh <mamadouleye2b@gmail.com>
2026-03-11 17:12:55 +0100

    end of setup

--- server.js
+++ server.js
@@ -1,26 +1,4 @@
-import Express from 'express';
-import http from 'http';
-import { Server } from 'socket.io';
+var Express = require('express');
+var app = Express();
 
-const app = Express();
-const server = http.createServer(app);
-const io = new Server(server);
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-        console.log('user disconnected');
-    });
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-        io.emit('chat message', msg);
-    });
-});
-
-server.listen(3000, () => {
-    console.log('listening on *:3000');
-});
-
+console.log('Server is running on port 3000');
