3dba5e5db756f69763144ab664e8f009866c8df4
Brawnyh <mamadouleye2b@gmail.com>
2026-03-11 17:02:19 +0100

    setup server

---
+++ .idea/test.js
@@ -0,0 +1,7 @@
+function main() {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${i}: Hello World`);
+    }
+}
+ 
+main();
---
+++ server.js
@@ -0,0 +1,26 @@
+import Express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = Express();
+const server = http.createServer(app);
+const io = new Server(server);
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
+
